35163387277af8b3c7d05fa29f8c796416d0531d
Eric McKinney <mckinney99@gmail.com>
2018-03-08 18:17:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function searchRepositories(){
   $.get(url).done(function(data) {
     const repoList = `${data.items.map(repo => { return (
       `<h3>${repo.name}</h3>
-      <p> <a href='${repo.html_url}'> ${repo.html_url}>'Repo URL:'</a></p>
+      <p> <a href='${repo.html_url}'>Repo URL:</a></p>
       <p><img src='${repo.owner.avatar_url}' height="40" width="40">  ${repo.owner.login}</p>
       <p>Description: ${repo.description}</p>
       <a href="#" data-owner="${repo.owner.login}" data-repository="${repo.name}" onclick="showCommits(this)">Show Commits</a><br>`)})
